19ee854950e3d9e6d2826ae2503839e2059f2067
Tomoaki Yagishita <tyagishi@gmail.com>
2021-02-23 16:47:12 +0900

    Initial Commit

---
+++ SDSOutlineViewExample/SDSOutlineViewExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SDSOutlineViewExampleApp.swift
+//
+//  Created by : Tomoaki Yagishita on 2021/02/23
+//  © 2021  SmallDeskSoftware
+//
+
+import SwiftUI
+
+@main
+struct SDSOutlineViewExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
