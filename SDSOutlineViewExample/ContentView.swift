ec24b3d9c45559fb8285486dbc6c96ddd77ddd86
Tomoaki Yagishita <tyagishi@gmail.com>
2021-02-23 18:45:56 +0900

    works with columns

--- SDSOutlineViewExample/ContentView.swift
+++ SDSOutlineViewExample/ContentView.swift
@@ -6,12 +6,62 @@
 //
 
 import SwiftUI
+import SwiftUIDebugUtil
 
 struct ContentView: View {
+    @StateObject var dataSource = OutlineSource()
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            Text("Hello, world!")
+                .background(Color.yellow)
+            Button(action: {
+                dataSource.childData[0].append(3)
+            }, label: {
+                Text("add new child")
+            })
+            SDSOutlineView(dataSource: dataSource,
+                           columnNames:[NSUserInterfaceItemIdentifier("name"), NSUserInterfaceItemIdentifier("comment")],
+                           delegate: nil)
+                .frame(maxWidth:.infinity, maxHeight: .infinity)
+                .background(Color.orange)
+                .debugBorder(.red, width: 3)
+        }
+        .padding()
     }
+}
+
+class OutlineSource: NSObject, NSOutlineViewDataSource, ObservableObject {
+    let parentData = [0,1,2]
+    @Published var childData = [
+        Range(10...19).map{$0},
+        Range(20...29).map{$0},
+        Range(30...39).map{$0}
+    ]
+    func outlineView(_ outlineView: NSOutlineView, isItemExpandable item: Any) -> Bool {
+        guard let intValue = item as? Int else { return false }
+        if intValue < parentData.count { return true }
+        return false
+    }
+    func outlineView(_ outlineView: NSOutlineView, numberOfChildrenOfItem item: Any?) -> Int {
+        if item == nil {
+            return parentData.count
+        }
+        return childData[0].count
+    }
+    func outlineView(_ outlineView: NSOutlineView, objectValueFor tableColumn: NSTableColumn?, byItem item: Any?) -> Any? {
+        guard let intValue = item as? Int else { return nil }
+        return String(intValue)
+    }
+    
+    func outlineView(_ outlineView: NSOutlineView, child index: Int, ofItem item: Any?) -> Any {
+        if item == nil {
+            return parentData[index]
+        }
+        guard let intValue = item as? Int,
+              intValue < parentData.count else { return "" }
+        return childData[intValue][index]
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ SDSOutlineViewExample/SDSOutlineView.swift
@@ -0,0 +1,49 @@
+//
+//  SDSOutlineView.swift
+//
+//  Created by : Tomoaki Yagishita on 2021/02/23
+//  © 2021  SmallDeskSoftware
+//
+
+import SwiftUI
+import AppKit
+
+struct SDSOutlineView: NSViewRepresentable {
+    
+    @ObservedObject var dataSource:OutlineSource
+    //var dataSource: OutlineSource
+    let columnNames:[NSUserInterfaceItemIdentifier]
+    var delegate: NSOutlineViewDelegate? = nil
+    
+    internal init(dataSource: OutlineSource, columnNames: [NSUserInterfaceItemIdentifier], delegate: NSOutlineViewDelegate? = nil) {
+        self.dataSource = dataSource
+        self.columnNames = columnNames
+        self.delegate = delegate
+    }
+
+    func makeNSView(context: Context) -> NSScrollView {
+        let scrollView = NSScrollView()
+        let outlineView = NSOutlineView()
+        scrollView.documentView = outlineView
+        outlineView.usesAlternatingRowBackgroundColors = true
+        for columnName in columnNames {
+            let newColumn = NSTableColumn(identifier: columnName)
+            outlineView.addTableColumn(newColumn)
+        }
+
+        outlineView.dataSource = dataSource
+        if let delegate = delegate {
+            outlineView.delegate = delegate
+        }
+        return scrollView
+    }
+    
+    func updateNSView(_ nsView: NSScrollView, context: Context) {
+        print("updateNSView is called")
+        guard let outlineView = nsView.documentView as? NSOutlineView else { return }
+        outlineView.reloadData()
+    }
+    
+    typealias NSViewType = NSScrollView
+}
+
